625f35552952ed29312f5afda4892fcaf8e5eb20
debashisbiswal <debashisbiswal@yahoo.com>
2026-03-20 23:28:33 +0530

    Modified dashboard and about us pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from "ngx-spinner";
 import { LoaderService } from './shared/services/loader.service';
+import { filter } from 'rxjs/operators';
 import { ColDef } from 'ag-grid-community';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    constructor(private toastrService: ToastrService, private spinner: NgxSpinnerService,
-      private loader: LoaderService,) {}
+    constructor(
+        private toastrService: ToastrService,
+        private spinner: NgxSpinnerService,
+        private loader: LoaderService,
+        private router: Router
+    ) {}
+
     ngOnInit() {
-      this.loader.isLoading.subscribe(res => {
-        if(res) {
-          this.spinner.show();
-        }
-        else {
-          this.spinner.hide();
-        }
-      })
-     }
+        // Scroll to top on navigation
+        this.router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe(() => {
+                window.scrollTo(0, 0);
+            });
+
+        this.loader.isLoading.subscribe(res => {
+            if(res) {
+                this.spinner.show();
+            }
+            else {
+                this.spinner.hide();
+            }
+        })
+    }
      columnDefs: ColDef[] = [
       { field: 'make' },
       { field: 'model' },
